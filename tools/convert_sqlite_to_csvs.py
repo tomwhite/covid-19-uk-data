6d82f693605175b56901965949c3261b09004e1a
Tom White <tom.e.white@gmail.com>
2020-05-04 15:12:51 +0100

    Fix export from sqlite to cases csv

--- tools/convert_sqlite_to_csvs.py
+++ tools/convert_sqlite_to_csvs.py
@@ -2,6 +2,7 @@
 
 # Generate all CSV files from the sqlite database
 
+import csv
 import pandas as pd
 import sqlite3
 
@@ -10,6 +11,10 @@ import pandas as pd
 
 from util import camel_to_hyphens, format_country, format_int
 
+def format_int_with_nan(val):
+    if math.isnan(val):
+        return "NaN"
+    return int(val)
 
 def convert(indicators_csv_file):
     indicators = pd.read_csv(indicators_csv_file)
@@ -45,7 +50,19 @@ if __name__ == "__main__":
     # export from sqlite to cases csv
     with sqlite3.connect('data/covid-19-uk.db') as conn:
         df = pd.read_sql('SELECT * FROM cases ORDER BY Date, Country, rowid', conn)
-        df.to_csv("data/covid-19-cases-uk.csv", index=False)
+
+        # don't use to_csv since pandas can't write NA ints
+        with open("data/covid-19-cases-uk.csv", "w") as csvfile:
+            csvfile.write("Date,Country,AreaCode,Area,TotalCases\n")
+            writer = csv.writer(csvfile, quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for (i, d) in df.to_dict("index").items():
+                writer.writerow([
+                    d["Date"],
+                    d["Country"],
+                    d["AreaCode"] if d["AreaCode"] is not None else "",
+                    d["Area"],
+                    format_int_with_nan(d["TotalCases"])
+                ])
 
     # convert indicators csv to totals csvs
     convert("data/covid-19-indicators-uk.csv")
